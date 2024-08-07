57da131fa2aeced3499aa077b78e79983116e820
Sai Kishor Kothakota <sai.kishor@pal-robotics.com>
2024-08-07 10:56:48 +0200

    update the logic to use the clock to initialize the last read and write cycle time

--- hardware_interface/src/actuator.cpp
+++ hardware_interface/src/actuator.cpp
@@ -44,6 +44,8 @@ const rclcpp_lifecycle::State & Actuator::initialize(
     switch (impl_->init(actuator_info, logger, clock_interface))
     {
       case CallbackReturn::SUCCESS:
+        last_read_cycle_time_ = clock_interface->get_clock()->now();
+        last_write_cycle_time_ = clock_interface->get_clock()->now();
         impl_->set_state(rclcpp_lifecycle::State(
           lifecycle_msgs::msg::State::PRIMARY_STATE_UNCONFIGURED,
           lifecycle_state_names::UNCONFIGURED));
@@ -229,7 +231,7 @@ return_type Actuator::read(const rclcpp::Time & time, const rclcpp::Duration & p
     impl_->get_state().id() == lifecycle_msgs::msg::State::PRIMARY_STATE_UNCONFIGURED ||
     impl_->get_state().id() == lifecycle_msgs::msg::State::PRIMARY_STATE_FINALIZED)
   {
-    last_read_cycle_time_ = rclcpp::Time(0, 0, time.get_clock_type());
+    last_read_cycle_time_ = time;
     return return_type::OK;
   }
   return_type result = return_type::ERROR;
@@ -253,7 +255,7 @@ return_type Actuator::write(const rclcpp::Time & time, const rclcpp::Duration & 
     impl_->get_state().id() == lifecycle_msgs::msg::State::PRIMARY_STATE_UNCONFIGURED ||
     impl_->get_state().id() == lifecycle_msgs::msg::State::PRIMARY_STATE_FINALIZED)
   {
-    last_write_cycle_time_ = rclcpp::Time(0, 0, time.get_clock_type());
+    last_write_cycle_time_ = time;
     return return_type::OK;
   }
   return_type result = return_type::ERROR;
--- hardware_interface/src/sensor.cpp
+++ hardware_interface/src/sensor.cpp
@@ -43,6 +43,7 @@ const rclcpp_lifecycle::State & Sensor::initialize(
     switch (impl_->init(sensor_info, logger, clock_interface))
     {
       case CallbackReturn::SUCCESS:
+        last_read_cycle_time_ = clock_interface->get_clock()->now();
         impl_->set_state(rclcpp_lifecycle::State(
           lifecycle_msgs::msg::State::PRIMARY_STATE_UNCONFIGURED,
           lifecycle_state_names::UNCONFIGURED));
@@ -205,7 +206,7 @@ return_type Sensor::read(const rclcpp::Time & time, const rclcpp::Duration & per
     impl_->get_state().id() == lifecycle_msgs::msg::State::PRIMARY_STATE_UNCONFIGURED ||
     impl_->get_state().id() == lifecycle_msgs::msg::State::PRIMARY_STATE_FINALIZED)
   {
-    last_read_cycle_time_ = rclcpp::Time(0, 0, time.get_clock_type());
+    last_read_cycle_time_ = time;
     return return_type::OK;
   }
   return_type result = return_type::ERROR;
